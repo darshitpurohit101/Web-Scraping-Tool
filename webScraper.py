6b174e07fbbc64c91c87728884d5b45f79185776
darshitpurohit101 <38689780+darshitpurohit101@users.noreply.github.com>
2020-01-27 21:04:23 +0530

    Update webScraper.py

--- webScraper.py
+++ webScraper.py
@@ -5,7 +5,7 @@ word = 'pizza'
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'
 
-url = "https://www.bing.com/images/search?q=ciling+fan&qs=n&form=QBILPG&sp=-1&ghc=1&pq=ciling+fan&sc=8-4&sk=&cvid=73D78D239D574921A293EF9725CD2F65"
+url = "https://www.bing.com/images/search?q=alabestron+artifact&qs=n&form=QBIR&sp=-1&pq=alabestron%20artifact&sc=1-19&cvid=430CC5C157164E57BB1CB66648AED15F&first=1&cw=1117&ch=738"
 headers={'User-Agent':user_agent,} 
 
 request=Request(url,None,headers) #The assembled request
@@ -17,61 +17,21 @@ response =urlopen(request)
 soup = BeautifulSoup(response,'html.parser')
 counter = 0
 
-for ul in soup.find_all('ul',{'class':'dgControl_list '}):
-    for li in soup.find_all('li'):
-        for images in soup.find_all('div',{'class':'img_cont hoff'}):
-            s = images.find('img')
-            img = s.get('data-src')
-            if img != None:
-                with open("Z:\pyimages\image" + str(counter) +".jpeg",'wb') as f:
-                    f.write(urlopen(img).read())
-                    counter += 1
-
-
-
-
-
-            
-            
-##for images in soup.find_all('div',{'class':'img_cont hoff'}):
-##    s = images.find('img')
-##    img = s.get('data-src')
-##    if img != None:
-##        
-##        with open("Z:\pyimages\image" + str(counter) +".jpeg",'wb') as f:
-##            f.write(urlopen(img).read())
-##            counter += 1
-##
-##
-##
-##
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
-     
-         
-
-    
-     
+for div_main in soup.find_all('div', {'id' : 'vm_c'}):
+#    print(div)
+    for div_external in soup.find_all('div', {'class' : 'dg_b'}):
+#        print(div)
+        for div_internal in soup.find_all('div', {'class': 'dgControl hover'}):
+#            print(div_internal)
+            for ul in soup.find_all('ul'):
+#                print(ul)
+                for li in soup.find_all('li'):
+#                    print(li)
+                    for image in soup.find_all('div', {'class' : 'iuscp varh'}):
+#                        print(image)
+                        s = image.find('img')
+                        img = s.get('data-src')
+                        if img != None:
+                           with open("\home\inpace\Documents\image" + str(counter) +".jpeg",'wb') as f:
+                                f.write(urlopen(img).read())
+                                counter += 1
