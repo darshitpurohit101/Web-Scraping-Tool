de75f67b2425a487c5e19bbdfb4d9b7c6e47ac31
darshitpurohit101 <38689780+darshitpurohit101@users.noreply.github.com>
2020-01-27 21:05:52 +0530

    Update webScraper.py

--- webScraper.py
+++ webScraper.py
@@ -1,8 +1,6 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 
-word = 'pizza'
-
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'
 
 url = "https://www.bing.com/images/search?q=alabestron+artifact&qs=n&form=QBIR&sp=-1&pq=alabestron%20artifact&sc=1-19&cvid=430CC5C157164E57BB1CB66648AED15F&first=1&cw=1117&ch=738"
